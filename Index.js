3e9616fbddcb356ac3dc342a4c9a4693a29212e1
saikat2007 <67913259+saikat2007@users.noreply.github.com>
2020-10-19 13:01:20 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="p5.play.js"></script>
+    <script src="monkey.js"></script>
+   
+</head>
+<body>
+  <script src="sketch.js"></script>
+</body>
+</html>
